4e9cf405d4586962d3d02fb4962e62ae6525baf0
kjhopp <56058382+kjhopp@users.noreply.github.com>
2021-09-22 12:35:43 -0400

    Add files via upload

---
+++ dns-revolver.ps1
@@ -0,0 +1,10 @@
+﻿param($network,$server)
+for ($i = 0; $i -lt 255; $i++)
+{
+
+$ip = "$network" + "." + "$i"
+
+$result = Resolve-DnsName -DnsOnly $ip -Server $server -ErrorAction Ignore | Select -Property NameHost, Name
+echo $result
+
+}
